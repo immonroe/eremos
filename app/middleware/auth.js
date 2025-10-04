7d38f4a34ef6c7d8294ce57cafbf88c2299a390e
immonroe <immanuelmonroe@gmail.com>
2025-10-04 14:31:37 -0400

    feat: add authentication middleware for clean MVC architecture

---
+++ app/middleware/auth.js
@@ -0,0 +1,11 @@
+// Authentication middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/auth');
+}
+
+module.exports = {
+    isLoggedIn
+};
